032e2d5c1fd074965b9721698f7ac024c3f1a506
robsonaugusto <robsonaugusto@users.noreply.github.com>
2025-02-03 22:36:03 +0000

    deploy: fa55c72bc1d98945b04d875dd0ecfd7140ec9529

--- src/test/java/utils/EmailUtils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import java.util.UUID;
-
-public class EmailUtils {
-    public static String generateRandomEmail() {
-        return "user_" + UUID.randomUUID().toString().substring(0, 8) + "@qa.com.br";
-    }
-}
